1207db79f3cf4b8c9ce8b7b84913bb11ceeb9617
Collin Vesel <cvesel217@gmail.com>
2025-03-11 15:38:25 -0500

    Added the serializer for the bookings and tweaked the other serializer for simplicity.

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import MenuItem
+from .models import MenuItem, Booking
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'price', 'inventory']
+        fields = "__all__"
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = "__all__"

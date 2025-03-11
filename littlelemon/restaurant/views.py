ae438de97de3286e370e9fb885bf321fae989926
Collin Vesel <cvesel217@gmail.com>
2025-03-11 18:18:22 -0500

    Added an auth check for the booking viewset and added a URL to get auth tokens for it. (I think something's off about how secure it is, though.)

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import MenuItem, Booking
 from .serializers import MenuItemSerializer, BookingSerializer
@@ -19,3 +20,4 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]

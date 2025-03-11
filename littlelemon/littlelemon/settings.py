17dd1ebda85717a6250815d666438343f4e4efc3
Collin Vesel <cvesel217@gmail.com>
2025-03-11 18:04:01 -0500

    Added auth URLs and put 'rest_framework.authtoken' in INSTALLED APPS since apparently it's required for tokens to work properly.

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurant',
     'rest_framework',
+    'rest_framework.authtoken',
     'djoser',
 ]
 
@@ -134,8 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "TokenAuthentication",
-        "SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ]
 }
 
